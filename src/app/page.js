79c3a3d42a4c723a40a62e491abd51df99e5e803
Jacob Winter <jacobwintr@gmail.com>
2025-06-12 19:30:46 -0400

    yuh

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <main>Liste.World coming soon...</main>
+    <main>
+      <h1>Liste.World coming soon...</h1>
+      <h2>Developed by Flow State Software LLC - Miami, FL</h2>
+    </main>
   );
 }
