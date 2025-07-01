e894e6328d3f19ad83ceeb753242dea35099db42
Jacob <jacobwintr@gmail.com>
2025-06-30 20:11:41 -0400

    added log in and sign up links to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Liste.World coming soon...</h1>
       <h2>Developed by <a href="https://www.flowstate.miami" target="_blank" rel="noreferrer">Flow State Software LLC - Miami, FL</a></h2>
+      <Link>log in</Link>
+      <Link>sign up</Link>
     </main>
   );
 }
