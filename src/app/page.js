38667120334051acda4222640be0b23a039da276
Jacob <jacobwintr@gmail.com>
2025-08-13 10:05:48 -0400

    updated domain

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Liste.World coming soon...</h1>
+      <h1>Liste.co coming soon...</h1>
       <h2>Developed by <a href="https://www.flowstate.miami" target="_blank" rel="noreferrer">Flow State Software LLC - Miami, FL</a></h2>
       <Link href="/auth/login">log in</Link><br />
       <Link href="/auth/signup">sign up</Link><br />
