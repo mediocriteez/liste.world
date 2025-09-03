1f191e585286096df55e23ac3cf868b9f9b6b0e1
Jacob Winter <jacobwintr@gmail.com>
2025-09-02 20:26:16 -0400

    started blog post styling

--- src/app/(public)/blog/[postID]/page.js
+++ src/app/(public)/blog/[postID]/page.js
@@ -7,22 +7,46 @@ const Page = async ({params}) => {
 
     const {postID} = await params
 
-    let title, body
+    let title, body, createdAt, updatedAt
 
     try{
-        const {data, error} = await supabase.from('posts').select('title, body').eq('id', postID).single()
+        const {data, error} = await supabase.from('posts').select('title, created_at, updated_at, body').eq('id', postID).single()
         if(error) throw error
         
         title = data.title
         body = data.body
+
+        const dateCreated = new Date(data.created_at)
+        const dateUpdated = new Date(data.updated_at)
+        createdAt = `${
+                    String(dateCreated.getMonth() + 1).padStart(2, "0")
+                } / ${
+                    String(dateCreated.getDate()).padStart(2, "0")
+                } / ${
+                    String(dateCreated.getFullYear()).slice(-2) // last 2 digits
+                }`
+        updatedAt = `${
+                    String(dateUpdated.getMonth() + 1).padStart(2, "0")
+                } / ${
+                    String(dateUpdated.getDate()).padStart(2, "0")
+                } / ${
+                    String(dateUpdated.getFullYear()).slice(-2) // last 2 digits
+                }`
     }catch(e){
         console.error(error?.message)
         console.error(error)
     }
 
     return(
-        <div className={classNamesToStr(['channelWidth', 'centered', css.contentContainer])}>
-            <h1>{title}</h1>
+        <div className={classNamesToStr(['channelMaxWidth', 'centered', css.contentContainer])}>
+            <div className={css.headerContent}>
+                <h1>{title}</h1>
+                <article className={css.contentMeta}>
+                    <p>Jacob Winter <span>- founder/ceo</span></p>
+                    <p>posted: {createdAt}</p>
+                    <p>last updated: {updatedAt}</p>
+                </article>
+            </div>
             <MDXRemote source={body} />
         </div>
     )
